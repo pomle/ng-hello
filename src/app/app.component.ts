a0ea3d7e15735562ec84fd9ed230810fcc923a8b
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-05 20:15:10 +0100

    Clean up HTML and render fixtures.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { ENTRIES } from '../fixtures/entries';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PrimaBlock';
+  entries = ENTRIES;
 }
