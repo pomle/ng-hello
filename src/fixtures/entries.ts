fa4b81ded4b3fc98a6da30a866bfd15638fa765b
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-05 20:14:30 +0100

    Add fixtures.

---
+++ src/fixtures/entries.ts
@@ -0,0 +1,22 @@
+export const ENTRIES = [
+	{
+		"address": "0x47c62777fa377e52b275832c01297433a26f83b0",
+		"balance": 1.00
+	},
+	{
+		"address": "0x6284475aced83fa9e1366b8381a16cd75324f947",
+		"balance": 25.00
+	},
+	{
+		"address": "0xe6accd697958c089474a48fcf78eeff63a98b0a1",
+		"balance": 50.00
+	},
+	{
+		"address": "0x638c52b243441bbe5efc70b15f335a604a50b6dc",
+		"balance": 41.00
+	},
+	{
+		"address": "0xae65934d6eeb5f134a096125d94e22fd38ed85dd",
+		"balance": 30.00
+	}
+]
